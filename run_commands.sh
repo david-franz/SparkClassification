41e98a50820b196e4c71a65e395e014aae696724
david-franz <52574893+david-franz@users.noreply.github.com>
2024-06-13 04:27:35 +1200

    Create run_commands.sh

---
+++ run_commands.sh
@@ -0,0 +1,13 @@
+hdfs dfs -mkdir /input/
+
+hdfs dfs -mkdir /output/
+
+hdfs dfs -put Android_Malware.csv /input
+
+mkdir compiled/
+
+javac -cp lib/spark -d compiled/ AndroidMalwareDetector.java
+
+jar cvf AndroidMalwareDetector.jar -C compiled/ .
+
+spark-submit --class malwareDetection.AndroidMalwareDetector --master yarn --deploy-mode cluster AndroidMalwareDetector.jar /input/Android_Malware.csv /output
